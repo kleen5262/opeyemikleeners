27fcb5c5ef1a4c7c213f6fc97f5582cf97fbd9d3
opeyemikleeners <opeyemi.fowose@gmail.com>
2020-03-24 17:43:23 +0100

    Javascript task uplaoded

---
+++ index.js
@@ -0,0 +1,9 @@
+let name="opeyemikleeners";
+console.log("my name is:"+name);
+const data=["HTML,CSS,JAVASCRIPT"];
+for(let i=0; i<=200; i++){
+	if(i%2==1){
+		data.push(i);
+	}
+}
+console.log(data);
